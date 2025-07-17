9d6c8f9a44ae74ba63c414dcf718f2847d10bd11
tnorwood612 <tmvnorwood@gmail.com>
2025-07-17 16:40:19 +0100

    test commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/ 
 export default defineConfig({
   plugins: [react()],
   base: 'https://tnorwood612.github.io/project-board'
